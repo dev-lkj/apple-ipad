c7d4059ae305dbdc8a9ddfd64f84b4fffc08ff9b
kjlee <2_kj@daum.net>
2023-06-07 22:54:57 +0900

    헤더 검색 영역 애니메이션 포커스 스타일 등 추가

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-// 드롭다운 메뉴 영역
+// 장바구니 드롭다운 메뉴 영역
 const basketStarterEl = document.querySelector('.header .basket-starter');
 const basketEl = basketStarterEl.querySelector('.basket');
 
@@ -28,4 +28,45 @@ function showBasket() { // 복잡한 개념을 추상화 하였음
 }
 function hideBasket() {
     basketEl.classList.remove('show')
+}
+
+// 검색 영역!
+const headerEl = document.querySelector('.header');
+const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]; // 전개연산자를 통한 얕은 복사
+const searchWrapEl = headerEl.querySelector('.search-wrap');
+const searchStarterEl = headerEl.querySelector('.search-starter');
+const searchCloserEl = searchWrapEl.querySelector('.search-closer');
+const searchShadowEl = searchWrapEl.querySelector('.shadow');
+const searchInputEl = searchWrapEl.querySelector('input');
+const searchDelayEls = [...searchWrapEl.querySelectorAll('li')];
+
+searchStarterEl.addEventListener('click', showSearch);
+searchCloserEl.addEventListener('click', hideSearch);
+searchShadowEl.addEventListener('click', hideSearch);
+
+function showSearch() {
+    headerEl.classList.add('searching');
+    document.documentElement.classList.add('fixed'); // document 최상위 문서 -> html
+    headerMenuEls.reverse().forEach(function (el, index) {
+        el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+    });
+    searchDelayEls.forEach(function (el, index) {
+        el.style.transitionDelay = index * .4 / searchDelayEls.length + 's';
+    });
+    setTimeout(function () {
+        searchInputEl.focus();
+    }, 600);
+    
+}
+function hideSearch() {
+    headerEl.classList.remove('searching');
+    document.documentElement.classList.remove('fixed');
+    headerMenuEls.reverse().forEach(function (el, index) {
+        el.style.transitionDelay = index * .4 / headerMenuEls.length + 's';
+    });
+    searchDelayEls.reverse().forEach(function (el, index) {
+        el.style.transitionDelay = index * .4 / searchDelayEls.length + 's';
+    });
+    searchDelayEls.reverse();
+    searchInputEl.value = '';
 }
